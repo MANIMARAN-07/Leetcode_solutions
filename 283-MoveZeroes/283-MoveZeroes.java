a9d33b44ca99cc60bc2db6cf53bee45f54d39119
Manimaran <manimaranayyappan555@gmail.com>
2026-08-12 11:14:42 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,15 @@
+// Last updated: 12/08/2026, 11:14:44
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+    }
+}
