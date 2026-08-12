f221467ba6f86bce6a51100daa5579da41ab6fee
Manimaran <manimaranayyappan555@gmail.com>
2026-08-12 11:13:37 +0530

    Create 424-LongestRepeatingCharacterReplacement

---
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,25 @@
+// Last updated: 12/08/2026, 11:13:39
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int ans = 0;
+        int n = s.length();
+        for (char c = 'A'; c <= 'Z'; c++) {
+            int i = 0, j = 0, replaced = 0;
+            while (j < n) {
+                if (s.charAt(j) == c) {
+                    j++;
+                } else if (replaced < k) {
+                    j++;
+                    replaced++;
+                } else if (s.charAt(i) == c) {
+                    i++;
+                } else {
+                    i++;
+                    replaced--;
+                }
+                ans = Math.max(ans, j - i);
+            }
+        }
+        return ans;
+    }
+}
