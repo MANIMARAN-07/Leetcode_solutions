3644c1d2c9d1f79b8a37863cf90de22c00ac0c9e
Manimaran <manimaranayyappan555@gmail.com>
2026-08-12 11:10:42 +0530

    Create 1741-SortArrayByIncreasingFrequency

---
+++ 1741-SortArrayByIncreasingFrequency/1741-SortArrayByIncreasingFrequency.java
@@ -0,0 +1,11 @@
+// Last updated: 12/08/2026, 11:10:44
+class Solution {
+public int[] frequencySort(int[] nums) {
+	Map<Integer, Integer> map = new HashMap<>();
+	Arrays.stream(nums).forEach(n -> map.put(n, map.getOrDefault(n, 0) + 1));
+	return Arrays.stream(nums).boxed()
+			.sorted((a,b) -> map.get(a) != map.get(b) ? map.get(a) - map.get(b) : b - a)
+			.mapToInt(n -> n)
+			.toArray();
+}
+}
